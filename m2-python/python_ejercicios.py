301bad2ed63150d106c7d24f5721025dd1526342
malala nyaina <nyainamalala71@gmail.com>
2023-07-12 15:21:08 +0200

    python

---
+++ m2-python/python_ejercicios.py
@@ -0,0 +1,19 @@
+variable1 = 10
+print(variable1)
+print(type(variable1))
+
+
+variable2 = 10.5
+print(type(variable2))
+
+persona_nombre = 'Malala'
+persona_apellido = "Ny Aina"
+
+print(type(persona_nombre))
+print(type(persona_apellido))
+
+
+senior = True
+junior = False
+
+print(type(senior))
